187f484427f67296e50e9f73afc535be13571d72
ChaseTerzian <chaseterzian@gmail.com>
2015-12-27 01:32:03 -0700

    trying to allow connection to perosnal app

--- app/controllers/testing_controller.rb
+++ app/controllers/testing_controller.rb
@@ -1,10 +1,20 @@
 class TestingController < ApplicationController
+	after_action :allow_iframe, only: :embed
 
 	def index
 	end
 
 	def show
 	end
+
+	def embed
+  end
+
+private
+
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
+  end
 	
 
 end
