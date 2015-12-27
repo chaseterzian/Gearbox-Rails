3012748d3ad2bf254c57a2d69ecdb385e2818e27
ChaseTerzian <chaseterzian@gmail.com>
2015-12-27 01:38:23 -0700

    attempt 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+	config.action_dispatch.default_headers['X-Frame-Options'] = "ALLOW-FROM https://gearbox-11beta.herokuapp.com"
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
