577ce0b178b6bbb60d05d528148bd950684cdf61
ChaseTerzian <chaseterzian@gmail.com>
2015-12-27 01:36:43 -0700

    undo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,6 @@
 class ApplicationController < ActionController::Base
-  after_action :allow_iframe, only: :embed
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  	def embed
-  end
-
-private
-
-  def allow_iframe
-    response.headers.except! 'X-Frame-Options'
-  end
 end
--- app/controllers/testing_controller.rb
+++ app/controllers/testing_controller.rb
@@ -1,5 +1,4 @@
 class TestingController < ApplicationController
-	after_action :allow_iframe, only: :embed
 
 	def index
 	end
@@ -7,14 +6,7 @@ class TestingController < ApplicationController
 	def show
 	end
 
-	def embed
-  end
 
-private
-
-  def allow_iframe
-    response.headers.except! 'X-Frame-Options'
-  end
 	
 
 end
